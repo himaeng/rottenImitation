7caa660e8296080a7b465f37e756fcc5d5149642
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2016-06-28 07:22:36 +0000

    initial adding files to rottentomatoesinitate repo

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,23 @@
+# This file is app/controllers/movies_controller.rb
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+  
+  def new
+      @movie = Movie.new
+  end
+  
+  def create
+    params.require(:movie)
+    params[:movie].permit(:title,:rating,:release_date)
+    # shortcut: params.require(:movie).permit(:title,:rating,:release_date)
+    # rest of code...
+  end
+end
